cadb2a56310a4838e3a8f4f4abeb25a9ff829ccd
Mahrukh <maharukh817@gmail.com>
2025-07-20 21:52:41 +0500

    add search functionality and complete this project

--- src/Pokemon.jsx
+++ src/Pokemon.jsx
@@ -5,7 +5,8 @@ const Pokemon = () => {
     const [pokemon,setPokemon] = useState([])
     const [loading,setLoading] = useState(true)
     const [error,setError] = useState(null)
-  const API = "https://pokeapi.co/api/v2/pokemon?limit=24";
+    const [search,setSearch] = useState("")
+  const API = "https://pokeapi.co/api/v2/pokemon?limit=25";
   const fetchPokemon = async () => {
     try {
       const res = await fetch(API);
@@ -30,9 +31,12 @@ const Pokemon = () => {
     fetchPokemon();
   }, []);
 
+  // search functionality
+  const searchData = pokemon.filter((currPokemon)=>currPokemon.name.toLowerCase().includes(search.toLowerCase()))
+
   if(loading){
     return(
-        <div>
+        <div className="center text-2xl font-extrabold">
             <h1>Loading...</h1>
         </div>
     )
@@ -52,11 +56,14 @@ const Pokemon = () => {
         <header>
             <h1 className="center heading">Let's Catch Pokemon</h1>
         </header>
+        <div className="input">
+          <input type="text" placeholder="search Pokemon" value={search} onChange={(e)=>setSearch(e.target.value)}/>
+        </div>
       </section>
       <div>
         <ul className="grid mt-10">
             {
-                pokemon.map((currPokemon)=>(
+                searchData.map((currPokemon)=>(
                     <PokemonCards key={currPokemon.id} pokemonData={currPokemon}/>
                 ))
             }
